f7fc2f498da3f72145576ed811638792b81402bd
RickyRockRat <gpib@rickyrockrat.net>
2021-04-08 09:08:00 -0600

    First pass at getting this running on a picozed with 2016.1

--- create_project.tcl
+++ create_project.tcl
@@ -1,14 +1,16 @@
 set dispScriptFile [file normalize [info script]]
 set scriptFolder [file dirname $dispScriptFile]
 
-create_project pcie-cdma-try1 $scriptFolder -part xc7z045ffg900-2
-set_property BOARD_PART xilinx.com:zc706:part0:1.0 [current_project]
+create_project pcie-cdma-try1 $scriptFolder -part xc7z030sbg485-1
 add_files -norecurse $scriptFolder/DesignWrapper.v
-add_files -fileset constrs_1 -norecurse $scriptFolder/zc706Constraints.xdc
+add_files -fileset constrs_1 -norecurse $scriptFolder/picozed_7z030_master_XDC_RevC_v2.xdc
 update_compile_order -fileset sources_1
 update_compile_order -fileset sim_1
 set_property ip_repo_paths  $scriptFolder/ip_repo/ [current_project]
 update_ip_catalog
-source $scriptFolder/zc706pl-ddr-pcie-cdma.2016.1.tcl
-launch_runs impl_1 -to_step write_bitstream -jobs 4
+source $scriptFolder/picozed030.2016.1.tcl
+set_param general.maxThreads 8
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+set run_list [list synth_1 impl_1] 
+foreach myrun $run_list {wait_on_run $myrun}
 
